f3916f3a94e4bdd899679ef01093f7bb683eb397
Nahush H Kumta <nahushhkumta@Nahushs-Laptop.local>
2025-03-29 22:33:30 -0400

    Adding periodic scraper code

---
+++ periodic-scraper/riperis_scraper.go
@@ -0,0 +1,17 @@
+package periodicscraper
+
+import "github.com/alistairking/bgpfinder"
+
+func risDriver(isRibs bool) {
+	ctx := setupContext()
+	logger := setupLogger()
+	db := setupDB(logger)
+
+	collectors, prevRuntimes, err := runDb(ctx, logger, db, ROUTEVIEWS)
+
+	if err != nil {
+		return
+	}
+
+	PeriodicScraper(ctx, logger, getRetryInterval(ROUTEVIEWS, isRibs), prevRuntimes, collectors, db, bgpfinder.NewRouteViewsFinder(), isRibs)
+}
